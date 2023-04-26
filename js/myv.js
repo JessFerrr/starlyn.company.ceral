ff4c58ae3e3ef0fb7a83d734edfc9b7f1c33e898
JessFerrr <105005241+JessFerrr@users.noreply.github.com>
2023-04-26 13:23:19 -0600

    initial commit

---
+++ js/myv.js
@@ -0,0 +1,11 @@
+
+
+function mostrar(){
+            document.getElementById('img').style.display = 'block';
+        }
+
+function ocultar(){
+            document.getElementById('img').style.display = 'none';
+        }
+
+        
